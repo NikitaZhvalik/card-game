44dd9830baa1b3352a46a1d96b3d8a4c1940436b
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-08-16 11:20:01 +1000

    04 - верстка 2 и 3 стр

--- js/renderStartPage.js
+++ js/renderStartPage.js
@@ -7,33 +7,35 @@ function saveLevel(newLevel) {
 }
 
 function loadLevel() {
-    return localStorage.getItem('level') ? JSON.parse(localStorage.getItem('level')) : 'eazy';
+    return localStorage.getItem('level') ? JSON.parse(localStorage.getItem('level')) : 'easy';
 }
 
 function renderStartPage() {
     const app = document.querySelector('#app')
     const html = `
-        <div class="main__start-page">
-            <h1 class="main__title">Выбери сложность</h1>
-            <div class="main__btns">
-                <button class="main__btn main__btn_eazy">
-                    1
-                </button>
-                <button class="main__btn main__btn_medium">
-                    2
-                </button>
-                <button class="main__btn main__btn_hard">
-                    3
-                </button>
+            <div class="wrapper">
+                <div class="main__start-page">
+                    <h1 class="main__title">Выбери сложность</h1>
+                    <div class="main__btns">
+                        <button class="main__btn main__btn_easy">
+                            1
+                        </button>
+                        <button class="main__btn main__btn_medium">
+                            2
+                        </button>
+                        <button class="main__btn main__btn_hard">
+                            3
+                        </button>
+                    </div>
+                        <button class="btn main__btn-start" type="submit">
+                            Старт
+                        </button>
+                </div>
             </div>
-            <button class="main__btn-start">
-                Старт
-            </button>
-        </div>
     `
     app.innerHTML = html
 
-    document.querySelector('.main__btn_eazy').addEventListener('click', () => saveLevel('eazy'))
+    document.querySelector('.main__btn_easy').addEventListener('click', () => saveLevel('easy'))
     document.querySelector('.main__btn_medium').addEventListener('click', () => saveLevel('medium'))
     document.querySelector('.main__btn_hard').addEventListener('click', () => saveLevel('hard'))
     document.querySelector('.main__btn-start').addEventListener('click', () => renderGame())
