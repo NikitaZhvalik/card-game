bdc5efae3cf6beb9bfb50b95343f9f6cbf99ca2a
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-02 22:21:26 +1000

    13: базовая настройка TS

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -12,6 +12,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
@@ -24,6 +29,9 @@ module.exports = {
         type: "asset/resource",
       },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
   optimization: {
     minimizer: [
