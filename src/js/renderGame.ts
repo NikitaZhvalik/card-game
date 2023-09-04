058e36a50b25280690b69d883f7f9615be70e03a
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-04 16:30:55 +1000

    20: добавление в package.son + renderGame

--- src/js/renderGame.ts
+++ src/js/renderGame.ts
@@ -1,29 +1,29 @@
-import { cardsGrade, cardsSuit } from "../helpers/const.js"
-import renderCards from "../components/renderGame/renderCards";
-import renderContainer from "../components/renderGame/renderContainer";
-import shuffleCards from "../components/renderGame/shuffleCards";
-import { renderResultGame } from "../components/renderGame/renderResultGame";
+import { cardsGrade, cardsSuit } from '../helpers/const.js'
+import renderCards from '../components/renderGame/renderCards'
+import renderContainer from '../components/renderGame/renderContainer'
+import shuffleCards from '../components/renderGame/shuffleCards'
+import { renderResultGame } from '../components/renderGame/renderResultGame'
 
 export default function renderGame() {
     type Cards = {
         cardsGrade: string;
         cardsSuit: string;
-    }
+    };
 
-    const level = JSON.parse(localStorage.getItem('level')!)   
+    const level = JSON.parse(localStorage.getItem('level')!)
 
     switch (level) {
         case 'easy':
             shuffleCards(3, cardsGrade, cardsSuit, renderPage)
-        break;
-    
+            break
+
         case 'medium':
             shuffleCards(6, cardsGrade, cardsSuit, renderPage)
-        break;
+            break
 
         case 'hard':
             shuffleCards(9, cardsGrade, cardsSuit, renderPage)
-        break;
+            break
     }
 
     const findCards = document.querySelectorAll('.play-card__back')
@@ -33,50 +33,84 @@ export default function renderGame() {
         })
     }
 
-    const cards = Array.from(document.querySelectorAll('.play-card'));
-    let resultGame :string
-    let resultIcon :string
-    
-    cards.forEach((card) => card.addEventListener('click', () => {
-        card.classList.add('open')
+    const cards = Array.from(document.querySelectorAll('.play-card'))
+    let resultGame: string
+    let resultIcon: string
 
-        let openCards = cards.filter((card) => card.classList.contains('open'))
-        openCards.forEach((card) => card.classList.add('win-card'))
-        const winCards = cards.filter((card) => card.classList.contains('win-card'))
+    cards.forEach((card) =>
+        card.addEventListener('click', () => {
+            card.classList.add('open')
 
-        if (winCards.length === cards.length) {
-            resultGame = 'Вы победили!'
-            resultIcon = './../../static/img/win-icon.svg'
-            stopTimer()
-            setTimeout(() => renderResultGame(resultGame, resultIcon, second, minute), 250)
-        }
+            let openCards = cards.filter((card) =>
+                card.classList.contains('open'),
+            )
+            openCards.forEach((card) => card.classList.add('win-card'))
+            const winCards = cards.filter((card) =>
+                card.classList.contains('win-card'),
+            )
 
-        if (openCards.length === 2) {
-            if (openCards[0].getAttribute('data-cardsGrade') === openCards[1].getAttribute('data-cardsGrade') && openCards[0].getAttribute('data-cardsSuit') === openCards[1].getAttribute('data-cardsSuit')) {
-                openCards.forEach((card) => card.classList.remove('open'));
-                return
-            }
-            if (openCards[0].getAttribute('data-cardsGrade') !== openCards[1].getAttribute('data-cardsGrade') || openCards[0].getAttribute('data-cardsSuit') !== openCards[1].getAttribute('data-cardsSuit')) {
-                openCards.forEach((card) => card.classList.remove('open'));
+            if (winCards.length === cards.length) {
+                resultGame = 'Вы победили!'
+                resultIcon = './../../static/img/win-icon.svg'
                 stopTimer()
-                resultGame = 'Вы проиграли!'
-                resultIcon = './../../static/img/lose-icon.svg'
-                setTimeout(() => renderResultGame(resultGame, resultIcon, second, minute), 250)
+                setTimeout(
+                    () =>
+                        renderResultGame(
+                            resultGame,
+                            resultIcon,
+                            second,
+                            minute,
+                        ),
+                    250,
+                )
             }
-            openCards = [];
-        }
-    }));
 
-    setTimeout (() => {
+            if (openCards.length === 2) {
+                if (
+                    openCards[0].getAttribute('data-cardsGrade') ===
+                        openCards[1].getAttribute('data-cardsGrade') &&
+                    openCards[0].getAttribute('data-cardsSuit') ===
+                        openCards[1].getAttribute('data-cardsSuit')
+                ) {
+                    openCards.forEach((card) => card.classList.remove('open'))
+                    return
+                }
+                if (
+                    openCards[0].getAttribute('data-cardsGrade') !==
+                        openCards[1].getAttribute('data-cardsGrade') ||
+                    openCards[0].getAttribute('data-cardsSuit') !==
+                        openCards[1].getAttribute('data-cardsSuit')
+                ) {
+                    openCards.forEach((card) => card.classList.remove('open'))
+                    stopTimer()
+                    resultGame = 'Вы проиграли!'
+                    resultIcon = './../../static/img/lose-icon.svg'
+                    setTimeout(
+                        () =>
+                            renderResultGame(
+                                resultGame,
+                                resultIcon,
+                                second,
+                                minute,
+                            ),
+                        250,
+                    )
+                }
+                openCards = []
+            }
+        }),
+    )
+
+    setTimeout(() => {
         const hiddenCards = document.querySelectorAll('.play-card__back')
         hiddenCards.forEach((card) => {
             card.classList.remove('none')
         })
         startTimer()
-    }, 1500) 
+    }, 1500)
     //todo поменяй таймер на 5000
 
-    function renderPage(selectedCards :Cards[]) {
+    function renderPage(selectedCards: Cards[]) {
         renderContainer()
         selectedCards.forEach((card) => {
             renderCards(card)
@@ -86,9 +120,9 @@ export default function renderGame() {
     // Поля таймера
     const timerSecond = document.querySelector('.next-page__second')
     const timerMinute = document.querySelector('.next-page__minute')
-    let second :number = 0
-    let minute :number = 0
-    let interval  :NodeJS.Timeout
+    let second: number = 0
+    let minute: number = 0
+    let interval: NodeJS.Timeout
 
     function startTimer() {
         goTimer()
@@ -103,7 +137,7 @@ export default function renderGame() {
     function goTimer() {
         second++
         if (second <= 9) {
-            if (timerSecond) timerSecond.textContent = "0" + second
+            if (timerSecond) timerSecond.textContent = '0' + second
         }
         if (second > 9) {
             if (timerSecond) timerSecond.textContent = String(second)
@@ -111,16 +145,24 @@ export default function renderGame() {
         if (second > 59) {
             minute++
             if (minute < 9) {
-                if (timerMinute) timerMinute.textContent = "0" + minute + "."
+                if (timerMinute) timerMinute.textContent = '0' + minute + '.'
                 second = 0
             }
             if (minute === 3) {
                 stopTimer()
                 resultGame = 'Вы проиграли!'
                 resultIcon = './../../static/img/lose-icon.svg'
-                setTimeout(() => renderResultGame(resultGame, resultIcon, second, minute), 250)
+                setTimeout(
+                    () =>
+                        renderResultGame(
+                            resultGame,
+                            resultIcon,
+                            second,
+                            minute,
+                        ),
+                    250,
+                )
             }
         }
     }
-
 }
