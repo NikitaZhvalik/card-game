c8ec043f3e855da737a3acf3776fb49183e4972e
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-08 15:47:19 +1000

    29: доделана логика игры

--- src/components/renderGame/renderCards.ts
+++ src/components/renderGame/renderCards.ts
@@ -5,7 +5,7 @@ type Cards = {
 
 export default function renderCards (card: Cards) {
     const htmlCards = `
-        <div class="play-card" data-cardsSuit="${card.cardsSuit}" data-cardsGrade="${card.cardsGrade}">
+        <div class="play-card disabled" data-cardsSuit="${card.cardsSuit}" data-cardsGrade="${card.cardsGrade}">
             <p class="play-card__text play-card__text_head" data-cardsSuit="${card.cardsSuit}" data-cardsGrade="${card.cardsGrade}">${card.cardsGrade}</p>
             <img class="play-card__back none" src="./../../static/img/card-back.svg" alt="" data-cardsSuit="${card.cardsSuit}" data-cardsGrade="${card.cardsGrade}">
             <img class="play-card__img play-card__img_head" src="./static/img/${card.cardsSuit}.svg" alt="" data-cardsSuit="${card.cardsSuit}" data-cardsGrade="${card.cardsGrade}">
